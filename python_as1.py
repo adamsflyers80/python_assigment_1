9a58efa12154864ddbe4b33f793006498af1c881
adamsflyers <adamsflyers80@gmail.com>
2020-04-23 14:40:01 +0300

    python_assignment1_commited

---
+++ python_as1.py
@@ -0,0 +1,8 @@
+passwd = "adem"
+name = input("Please write your names: ")
+if passwd == name:
+        print("Hello, Adem! The password is : @12")
+    else:
+            print("Hello", name, "! See you later")
+
+            
