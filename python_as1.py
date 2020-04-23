e36fcb984b6acd6c7c43aa2cf32f04bba86c9c6b
adamsflyers80 <63156981+adamsflyers80@users.noreply.github.com>
2020-04-23 15:03:52 +0300

    names corrected

--- python_as1.py
+++ python_as1.py
@@ -1,8 +1,10 @@
-passwd = "adem"
-name = input("Please write your names: ")
+passwd = "Adem"
+name = input("Please write your name: ")
 if passwd == name:
         print("Hello, Adem! The password is : @12")
     else:
             print("Hello", name, "! See you later")
+                
+                
 
             
